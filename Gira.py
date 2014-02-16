626f461ca37a2f2bd3654b96b0d77ca6705ad114
cronosdead <alexmadden07@gmail.com>
2014-02-15 20:57:32 -0600

    Update Gira.py

--- Gira.py
+++ Gira.py
@@ -1,7 +1,7 @@
 from Pila import *
 
 def Resolv(A, X, Clock):
-	B = [0, 0, 0, 0] # hay alguna otra forma de obtener este vector? 
+	B = [0] * len(X) # creo es la forma más adecuada
 	for i in range(len(X)):
 		B[i] = X[i]
 	#sentido manecillas de reloj
